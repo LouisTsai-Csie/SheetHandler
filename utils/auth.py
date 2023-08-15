fd6a94385752d4fa6f50e081c8d3030aa5028c35
LouisTsai <q1030176@gmail.com>
2023-08-16 00:19:53 +0800

    fix(auth): update secret management to enable streamlit cloud

--- utils/auth.py
+++ utils/auth.py
@@ -1,11 +1,23 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from config import link
+import streamlit as st
 
-def authenticate(url = link.BASESHEET):
+def authenticate(url):
     gss_scopes = ['https://spreadsheets.google.com/feeds']
-    path = './config.json'
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(path, gss_scopes)
+    credentials_info = {
+        "type": "service_account",
+        "project_id": st.secrets['project_id'],
+        "private_key_id": st.secrets['private_key_id'],
+        "private_key": st.secrets['private_key'],
+        "client_email": st.secrets['client_email'],
+        "client_id": st.secrets['client_id'],
+        "auth_uri": st.secrets['auth_uri'],
+        "token_uri": st.secrets['token_uri'],
+        "auth_provider_x509_cert_url": st.secrets['auth_provider_x509_cert_url'],
+        "client_x509_cert_url": st.secrets['client_x509_cert_url']
+    }
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_info, gss_scopes)
     gss_client = gspread.authorize(credentials)
     sheet = gss_client.open_by_url(url)
     return sheet
